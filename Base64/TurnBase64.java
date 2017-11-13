983cc1ebc809d669a4fdbdf5dae2a466fcbcd296
LiuFreedom <542877846@qq.com>
2017-11-13 17:47:51 +0800

    Add files via upload

---
+++ Base64/TurnBase64.java
@@ -0,0 +1,45 @@
+package com.kelan.powerapp.utils;
+
+import android.graphics.Bitmap;
+import android.util.Base64;
+
+import com.kelan.powerapp.encode.BASE64Encoder;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Ocean Liu on 2017/10/20/0020.
+ */
+
+public class TurnBase64 {
+    /**
+     * 转码base64
+     *
+     * @param bitmap
+     * @return
+     */
+    public static String bitmaptoString(Bitmap bitmap) {
+        // 将Bitmap转换成字符串
+        String string = null;
+        ByteArrayOutputStream bStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 80, bStream);
+        byte[] bytes = bStream.toByteArray();
+        string = Base64.encodeToString(bytes, Base64.DEFAULT);
+        return string;
+    }
+
+    public static String encodeBase64Files(String path) throws Exception {
+        File file = new File(path);
+        FileInputStream inputFile = new FileInputStream(file);
+        byte[] buffer = new byte[(int) file.length()];
+        inputFile.read(buffer);
+        inputFile.close();
+        return new BASE64Encoder().encode(buffer);
+    }
+}
